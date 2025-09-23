52a27039336cf03222722fd5a9fba94fa379117d
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-09-23 18:47:01 +0400

    Modify main.py and docker-compose.yaml for multipy launch

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app = FastAPI(title="API V1")
 app.include_router(api_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
