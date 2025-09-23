9c044c48ef3145033a0f0de693b8d8354c33061c
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-09-23 10:26:32 +0400

    Add admin_role column in model: models/users.py

--- models/users.py
+++ models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from models.hotels import Base
 
@@ -8,4 +8,5 @@ class Users(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String, nullable=False)
+    admin_role: Mapped[bool] = mapped_column(Boolean, server_default="False")
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
