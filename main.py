3e0955680bfb1d8f8b33d3e15b1f18eb3b191550
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-09-23 10:58:44 +0400

    Include auth user and users routers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+#routers
 from routers.bookings.router import router as bookings_router
+from routers.auth.router import router as auth_router
 from routers.users.router import router as user_router
 
 import uvicorn
@@ -7,6 +9,7 @@ import uvicorn
 app = FastAPI()
 
 app.include_router(bookings_router)
+app.include_router(auth_router)
 app.include_router(user_router)
 
 if __name__ == "__main__":
