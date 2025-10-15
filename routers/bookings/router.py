5450674be36976505964895b5d4d79db39c55681
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-15 12:05:08 +0400

    routers/bookings/router.py: add_booking method

--- routers/bookings/router.py
+++ routers/bookings/router.py
@@ -1,14 +1,30 @@
 from fastapi import APIRouter, Depends
 from routers.bookings.dao import BookingsDAO
 from routers.bookings.schemas import SBooking
+from routers.bookings.exceptions import CannotBookedException
 from routers.users.depends import get_user_from_sub
+from datetime import date
+from models.users import Users
 
 router = APIRouter()
 
 
 @router.post("/create")
-def add_booking():
-    pass
+async def add_booking(
+    room_id: int,
+    date_from: date,
+    date_to: date,
+    user: Users = Depends(get_user_from_sub)
+    ):
+    booking = await BookingsDAO.add(
+        room_id=room_id,
+        user_id=user.id,
+        date_to=date_to,
+        date_from=date_from
+    )
+    if not booking:
+        raise CannotBookedException
+
 
 
 @router.get("/get")
