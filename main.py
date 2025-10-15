88ea118a5c7dc56139ba0c0f6b28c701cbf6da2a
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-15 12:02:03 +0400

    main.py: Add CORS and Middleware. Add frontend and upload images routers

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from routers.__init__ import router as api_router
+from routers.__init__ import frontend_router
+from routers.__init__ import upload_images_router
+from config import env_config
 
 import uvicorn
 
 app = FastAPI(title="API V1")
 
 app.include_router(api_router)
+app.include_router(frontend_router)
+app.include_router(upload_images_router)
+app.mount(path="/statics", app=StaticFiles(directory="statics"), name="statics")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=env_config.ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
