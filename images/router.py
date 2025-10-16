bfadc8ea6ba2a7e2c05426ffcb1e132f19b28235
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-16 09:33:58 +0400

    images/router.py: Add processing image router

--- images/router.py
+++ images/router.py
@@ -1,4 +1,5 @@
 from fastapi import UploadFile, APIRouter
+from tasks.tasks import process_pic
 import shutil
 
 router = APIRouter()
@@ -9,5 +10,7 @@ async def add_hotel_image(
         file: UploadFile,
         name: int
 ):
+    image_path = f"statics/images/{name}.webp"
     with open(f"statics/images/{name}.webp", "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
+    process_pic.delay(image_path)
