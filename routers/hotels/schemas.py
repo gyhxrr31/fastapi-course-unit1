cdb8f3300abf86ea5dd427a509dea65ebc3c0464
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-16 09:38:01 +0400

    routers/hotels/schemas.py: remove useless line

--- routers/hotels/schemas.py
+++ routers/hotels/schemas.py
@@ -5,18 +5,12 @@ from datetime import date
 
 class BookingWithRoomResponse(BaseModel):
     id: int
-    room_id: int
-    user_id: int
-    date_from: date
-    date_to: date
-    price: int
-    total_cost: int
-    total_days: int
-    # Данные номера
-    room_name: str
-    room_description: Optional[str]
-    room_services: List[str]
-    room_image_id: int
+    name: str
+    location: str
+    services: List[str]
+    rooms_quantity: int
+    image_id: int
+    rooms_left: int
 
     model_config = ConfigDict(from_attributes=True)
 
