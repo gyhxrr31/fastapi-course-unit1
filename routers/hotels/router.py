1c5ea40ebc06e9501b5bb949f39f936c8574f1a9
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-16 09:37:21 +0400

    routers/hotels/router.py: refactor and adding cache decorator

--- routers/hotels/router.py
+++ routers/hotels/router.py
@@ -2,17 +2,19 @@ from fastapi import APIRouter
 from routers.hotels.dao import HotelDAO
 from routers.hotels.schemas import BookingWithRoomResponse
 from datetime import date
+from fastapi_cache.decorator import cache
+from typing import List
 
 router = APIRouter()
 
 
-
-@router.get("/{hotel_name}")
+@cache(expire=60)
+@router.get("/{location}")
 async def get_hotels_by_name(
         location: str,
         date_from: date,
         date_to: date
-) -> BookingWithRoomResponse:
+) -> List[BookingWithRoomResponse]:
     return await HotelDAO.find_hotels_with_available_rooms(
         location,
         date_from,
