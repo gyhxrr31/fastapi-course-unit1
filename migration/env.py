312902a5b2f381a7b27a29b61d0b2e9450c084db
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-16 09:33:30 +0400

    migrations/env.py: Change DB_URL to container hostname

--- migration/env.py
+++ migration/env.py
@@ -18,7 +18,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", env_config.DB_URL_LOCAL)
+config.set_main_option("sqlalchemy.url", env_config.DB_URL_CONTAINER)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
