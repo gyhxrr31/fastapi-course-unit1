210858f28c44a6edfcf02c51cf25031bfa256fdb
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-16 09:09:04 +0400

    config.py: Add REDIS url

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class EnvConfig(BaseSettings):
     PRIVATE_KEY: str
     ALGORITHM: str
     ORIGINS: list
+    REDIS_URL: str
     model_config = SettingsConfigDict(env_file=".env", extra="ignore") #игнорируем лишние переменные
 
 
