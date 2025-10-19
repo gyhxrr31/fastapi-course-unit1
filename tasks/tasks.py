afdeb18ccd80e5a077e9c979eedce59859fd3e3e
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-19 15:24:30 +0400

    tasks/tasks.py: Add new email send task

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,7 +1,13 @@
+from config import env_config
 from tasks.celery_config import celery
 from PIL import Image
 from loguru import logger
 from pathlib import Path
+from pydantic import EmailStr
+import smtplib
+
+from tasks.email_templates import create_booking_confirmation_template
+
 
 @celery.task
 def process_pic(
@@ -14,3 +20,15 @@ def process_pic(
     image_resized_200_100 = image.resize(size=(200, 100))
     image_resized_1000_500.save(f"statics/images/resized_1000_500{image_path.name}")
     image_resized_200_100.save(f"statics/images/resized_200_100{image_path.name}")
+
+
+@celery.task
+def send_booking_confirmation_email(
+        booking: dict,
+        email_to: EmailStr
+):
+    msg_content = create_booking_confirmation_template(booking, email_to)
+
+    with smtplib.SMTP_SSL(env_config.SMTP_HOST, env_config.SMTP_PORT) as server:
+        server.login(env_config.SMTP_USER, env_config.SMTP_PWD)
+        server.send_message(msg_content)
