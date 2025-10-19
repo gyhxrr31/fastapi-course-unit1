09e1610cb0dcea734aebabd42b639ec5213d6508
codehack13 <ivanov.ivan.ivanovich1968@gmail.com>
2025-10-19 15:24:09 +0400

    routers/bookings/router.py: Add delete method and send email message for verification

--- routers/bookings/router.py
+++ routers/bookings/router.py
@@ -1,29 +1,44 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form
 from routers.bookings.dao import BookingsDAO
 from routers.bookings.schemas import SBooking
 from routers.bookings.exceptions import CannotBookedException
 from routers.users.depends import get_user_from_sub
 from datetime import date
 from models.users import Users
+from tasks.tasks import send_booking_confirmation_email
+from routers.bookings.utils import sqlalchemy_to_dict
 
 router = APIRouter()
 
 
 @router.post("/create")
 async def add_booking(
-    room_id: int,
-    date_from: date,
-    date_to: date,
+    room_id: int = Form(...),
+    date_from: date = Form(...),
+    date_to: date = Form(...),
     user: Users = Depends(get_user_from_sub)
     ):
-    booking = await BookingsDAO.add(
+    booking = await BookingsDAO.add_bookings(
         room_id=room_id,
         user_id=user.id,
         date_to=date_to,
         date_from=date_from
     )
+    send_booking_confirmation_email.delay(sqlalchemy_to_dict(booking), user.email)
     if not booking:
         raise CannotBookedException
+
+
+@router.delete("/delete")
+async def delete_booking_by_user(
+        user: Users = Depends(get_user_from_sub),
+        room_id: int = Form(...)
+) -> None:
+   return await BookingsDAO.delete_booking(
+       user_id=user.id,
+       room_id=room_id
+   )
+
 
 
 
